18873b43764e0217b3d5efc50066f40e829449bc
HIGASHIYAMA Yasunori <higashiyama.yasunori@gmail.com>
2021-08-18 00:17:56 +0900

    実装修正

--- api/src/main/java/jp/co/neosystem/kadai/api/mapper/TaskMapper.java
+++ api/src/main/java/jp/co/neosystem/kadai/api/mapper/TaskMapper.java
@@ -2,12 +2,12 @@ package jp.co.neosystem.kadai.api.mapper;
 
 import jp.co.neosystem.kadai.api.entity.TaskEntity;
 import org.apache.ibatis.annotations.Mapper;
-import org.apache.ibatis.annotations.Select;
 
 import java.util.List;
 
 @Mapper
 public interface TaskMapper {
-	@Select("select * from TASK_MANAGEMENT")
-	List<TaskEntity> findAll();
+	List<TaskEntity> select(boolean all, String search, boolean todo, String sort, String order, int limit, int offset);
+
+	long count(boolean all, String search, boolean todo);
 }
