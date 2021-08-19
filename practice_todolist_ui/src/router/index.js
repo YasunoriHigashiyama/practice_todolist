b6099db73721e23415883c3ddaabf1ff5d47045e
Suechon <ss.710@i.softbank.jp>
2021-08-19 16:25:52 +0900

    mockの設定戻した

--- practice_todolist_ui/src/router/index.js
+++ practice_todolist_ui/src/router/index.js
@@ -1,6 +1,5 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
-// import Home from '../views/Home.vue'
 import TodoList from "../views/TodoList"
 
 Vue.use(VueRouter)
@@ -11,19 +10,11 @@ const routes = [
     name: 'TodoList',
     component: TodoList
   },
-  // {
-  //   path: '/about',
-  //   name: 'About',
-  //   // route level code-splitting
-  //   // this generates a separate chunk (about.[hash].js) for this route
-  //   // which is lazy-loaded when the route is visited.
-  //   component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
-  // }
 ]
 
 const router = new VueRouter({
   mode: 'history',
-  routes
+  routes: routes,
 })
 
 export default router
